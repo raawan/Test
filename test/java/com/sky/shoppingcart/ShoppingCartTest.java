de1f070cbd3a5aa82eb660ddfd8016f6d7ea32bc
Fulzele <Minal.Fulzele@nw.win.int.kn>
2018-10-15 10:30:51 +0100

    passed the first failing test

--- test/java/com/sky/shoppingcart/ShoppingCartTest.java
+++ test/java/com/sky/shoppingcart/ShoppingCartTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Created by Minal.Fulzele on 15/10/2018.
@@ -15,6 +16,6 @@ public class ShoppingCartTest {
     public void GIVEN_OneHeadphoneInShoppingCart_THEN_DiscountedPriceShouldBe_105() {
         ShoppingCart shoppingCart =  new ShoppingCart(new Item("Headphones"));
         BigDecimal discountedPrice = shoppingCart.getItem("Headphones").getDiscountedPrice();
-        assertEquals(105,discountedPrice.doubleValue(),0);
+        assertTrue(new BigDecimal("105").equals(discountedPrice));
     }
 }
